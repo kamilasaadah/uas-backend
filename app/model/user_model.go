e2d23243c588f51bbf78d9afe3105495659dba64
kamila <kamilasaadah8@gmail.com>
2025-11-25 12:22:34 +0700

    feat(auth): add login request & response models (FR-001)

--- app/model/user_model.go
+++ app/model/user_model.go
@@ -1,1 +1,18 @@
 package model
+
+// ======================= LOGIN REQUEST =======================
+
+type LoginRequest struct {
+	Username string `json:"username" validate:"required"` // boleh username/email
+	Password string `json:"password" validate:"required"`
+}
+
+// ======================= LOGIN RESPONSE =======================
+
+type AuthUserResponse struct {
+	ID          string   `json:"id"`
+	Username    string   `json:"username"`
+	FullName    string   `json:"full_name"`
+	Role        string   `json:"role"`
+	Permissions []string `json:"permissions"`
+}
